2dbd24289606d7c01e85f274ffd001168984e655
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-06-24 01:11:11 +0200

    Set data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
+  after_initialize :set_defaults
+
   validates :title, :body, :main_image, :thumb_image, presence: true
+
+  def set_defaults
+    self.main_image ||= 'https://via.placeholder.com/600x400'
+    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+  end
 end
