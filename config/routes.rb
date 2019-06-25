c1d96a08d26b8560173790c103a489a0d6eda1cc
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-06-25 22:13:30 +0200

    Customize the routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get 'about-me' => 'pages#about'
   get 'contact' => 'pages#contact'
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'account', sign_up: 'signup' }
   root 'pages#home'
 end
