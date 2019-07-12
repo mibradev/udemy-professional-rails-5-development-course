57a7a30185f0614b6e7e0053267af6876f0a14f3
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-07-12 22:25:29 +0200

    Integrate content helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,13 @@ module ApplicationHelper
       " " +
       link_to('Signup', new_user_registration_path)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to('Logout', destroy_user_session_path, method: :delete)
+    end
+  end
+
+  def utm_source_helper
+    if session[:utm_source]
+      content_tag(:p, "Thanks for visiting from #{session[:utm_source]}", class: 'utm-source-greeting')
     end
   end
 end
