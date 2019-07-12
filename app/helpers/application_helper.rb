41fb40cbc79853e54486403de73f9d128f14f69e
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-07-12 22:00:05 +0200

    Integrate view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+    unless user_signed_in?
+      link_to('Login', new_user_session_path) +
+      " " +
+      link_to('Signup', new_user_registration_path)
+    else
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    end
+  end
 end
