d8ca4e47f75bcff46ed88fdfe543861c7541201b
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-07-29 11:07:02 +0200

    Implement home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(html_class: '')
     unless user_signed_in?
-      link_to('Login', new_user_session_path) +
+      link_to('Login', new_user_session_path, class: html_class) +
       " " +
-      link_to('Signup', new_user_registration_path)
+      link_to('Signup', new_user_registration_path, class: html_class)
     else
-      link_to('Logout', destroy_user_session_path, method: :delete)
+      link_to('Logout', destroy_user_session_path, method: :delete, class: html_class)
     end
   end
 
