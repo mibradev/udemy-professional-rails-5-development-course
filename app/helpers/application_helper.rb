d80998bd1e013928bcef85219d657314fcaa3e19
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-01 03:27:03 +0200

    Improve shared navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def navbar_items
+    items = [
+      { name: 'Home', path: root_path },
+      { name: 'About', path: about_me_path },
+      { name: 'Contact', path: contact_path },
+      { name: 'Blog', path: blogs_path },
+      { name: 'Portfolio', path: portfolios_path }
+    ]
+
+    unless user_signed_in?
+      items.push(
+        { name: 'Login', path: new_user_session_path },
+        { name: 'Signup', path: new_user_registration_path }
+      )
+    else
+      items << { name: 'Logout', path: destroy_user_session_path, method: :delete }
+    end
+  end
 end
