06921c47f35fa85ac35bb73f248d6733a1a04ecc
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-10 09:33:28 +0200

    Improve blog status button style

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,8 @@ module BlogsHelper
   def comments_pagination
     link_to_next_page @comments, 'More', param_name: :comment_page, remote: true
   end
+
+  def status_button_class(status)
+    ['btn btn-sm', { draft: 'btn-secondary', published: 'btn-success' }[status.to_sym]]
+  end
 end
